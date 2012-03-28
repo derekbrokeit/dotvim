547ffbaabe9b5fa38e3b4b691833371ff95e303f
Derek (momo-castella) <derekathomas@gmail.com>
2012-03-28 16:53:17 +0900

    Better updatemodules.sh which allows for init and commits

--- updatemodules.sh
+++ updatemodules.sh
@@ -1,10 +1,17 @@
-#!/bin/bash
+#!/bin/sh
 
-pushd . > /dev/null
+if [[ ! -f ~/.vim/.submoduleinit ]] ; then
+  # make sure we initialize the submodules
+  echo "## -- initializing submodules -- ##"
+  git submodule update --init
+  touch ~/.vim/.submoduleinit
+fi
 
-git submodule init
-git submodule update
+# now, update all those submodules
+echo "## -- pull all submodules -- ##"
 git submodule foreach git pull origin master
-git submodule update
+git status | grep "new commits" | cut -d ' ' -f 4 | xargs git add
+echo ""
+echo "git commit"
+git commit
 
-popd > /dev/null
