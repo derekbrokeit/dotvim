5a6b373270c9f975d2e302db7e3b9d40bf413fd9
Derek (momo-castella) <derekathomas@gmail.com>
2012-04-11 11:29:15 +0900

    Remake Command-T after update

--- updatemodules.sh
+++ updatemodules.sh
@@ -15,3 +15,19 @@ echo ""
 echo "git commit "
 git commit
 
+# Attempt to update commandT
+pushd . > /dev/null
+ctdir=$HOME/.vim/bundle/commandT/ruby/command-t
+ctfile=$ctdir/extconf.rb
+if [[ -f $ctfile ]] ; then
+  cd $ctdir
+  echo "## -- Command-T: generate Makefile -- ##"
+  ruby $ctfile
+  echo "make ($PWD)"
+  make
+  echo "    * exit: $?"
+else
+  echo "CommandT not was not able to rebuild"
+fi
+popd > /dev/null
+
